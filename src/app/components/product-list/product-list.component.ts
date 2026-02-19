89b9081b78df8fdd9232169cb5fdb8578aaca650
fdipuccio <32079226+fdipuccio@users.noreply.github.com>
2026-02-19 21:55:06 +0100

    Add product list component for e-commerce application

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product.model';
+
+@Component({
+    selector: 'app-product-list',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+    products: Product[] = [];
+    searchQuery: string = '';
+
+    constructor(private productService: ProductService) {}
+
+    ngOnInit(): void {
+        this.loadProducts();
+    }
+
+    loadProducts() {
+        this.productService.getProducts().subscribe(products => {
+            this.products = products;
+        });
+    }
+}
